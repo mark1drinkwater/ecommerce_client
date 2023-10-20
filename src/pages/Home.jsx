3e05246b2641623cea67ef4c7b810b09a5526ade
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-20 03:51:54 +0200

    re-enabled announcement bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import Footer from '../components/Footer';
 const Home = () => {
   return (
     <div>
-      {/* <Announcement /> */}
+      <Announcement />
       <Navbar />
       <Slider />
       <Categories />
